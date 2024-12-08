5713be5c5ac71338706829e04f01dcaf9264e4d5
Nikhil3522 <nikhiljha3522@gmail.com>
2024-12-09 02:54:37 +0530

    all share icon

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './components/Home';
 import reportWebVitals from './reportWebVitals';
 import Navigator from './routes/Navigator';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navigator />
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
   </React.StrictMode>
 );
 
